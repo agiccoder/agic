9df0c9534898ee342d3709978cf3a7fe98cc93dd
agiccoder <105564506+agiccoder@users.noreply.github.com>
2022-05-14 22:42:44 +0300

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,81 +1,36 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 import seaborn as sns
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-import altair as alt
+import plotly.express as px
+def get_data():
+    url = "https://raw.githubusercontent.com/Hernan4444/MyAnimeList-Database/master/data/anime.csv"
+    return pd.read_csv(url)
+df_data = get_data()
+st.write(f'### Here is our filtered data')
+df_data = df_data.filter(
+    items=['Name', 'Score', 'Type', 'Source', 'Genres', 'Episodes', 'Duration', 'scored_by', 'Completed', 'Dropped'])
+df_data.head()
+df_data
+df_data['Completed'] = df_data['Completed'].astype(int)
+tips = px.data.tips()
+figure = px.histogram(df_data, y="Type", title='Distribution of anime by type')
+figure
+figure = px.histogram(df_data, x="Type", color="Source", title='Distribution of anime by type including the source')
+figure
+fig, ax = plt.subplots()
+sns.scatterplot(data=df_data, x='Score', y='Completed', hue='Type', style='Type')
+ax.set_ylim(bottom=0, top=1000000)
+st.pyplot(fig)
+score_chosen = st.slider('Completed',min_value=0, max_value=1000000)
+b=df_data.sort_values(by=['Completed'])
+c = b.loc[b['Completed']>score_chosen]
+fig, ax=plt.subplots(1,1, figsize=(10,10))
+sns.scatterplot(data=c, x="Episodes", y='Completed')
+st.pyplot(fig)
+st.write("Some magic right here")
 
-with st.echo(code_location='below'):
-    """
-    ## Hello, World!
-    """
-
-
-    def print_hello(name="World"):
-        st.write(f"### Hello, {name}!")
-
-
-    name = st.text_input("Your name", key="name", value="Anonymous")
-    print_hello(name)
-
-    """
-    ## Добавим графики
-    Чтобы заработали библиотеки seaborn и altair, нужно добавить в проект файл 
-    `requirements.txt` с такими строчками:
-    
-        seaborn
-        altair
-    """
-
-
-    a = st.slider("a")
-    x = np.linspace(-6, 6, 500)
-    df = pd.DataFrame(dict(x=x, y=np.sin(a * x)))
-    fig, ax = plt.subplots()
-    sns.lineplot(data=df, x="x", y="y", ax=ax)
-    st.pyplot(fig)
-
-    """
-    ## Немного анализа данных
-    """
-
-
-    @st.cache
-    def get_data():
-        data_url = (
-            "https://github.com/Godoy/imdb-5000-movie-dataset/raw/"
-            "master/data/movie_metadata.csv"
-        )
-        return (
-            pd.read_csv(data_url)
-            .dropna(subset=["title_year"])
-            .assign(
-                title_year=lambda x: pd.to_datetime(
-                    x["title_year"], format="%Y"
-                )
-            )
-        )
-
-
-    df = get_data()
-
-    director = st.selectbox(
-        "Director", df["director_name"].value_counts().iloc[:10].index
-    )
-
-    df_selection = df[lambda x: x["director_name"] == director]
-    df_selection
-
-    chart = (
-        alt.Chart(df_selection)
-        .mark_circle()
-        .encode(x=alt.X("title_year:T"), y="imdb_score", tooltip="movie_title")
-    )
-
-    st.altair_chart(
-        (
-            chart
-            + chart.transform_loess("title_year", "imdb_score").mark_line()
-        ).interactive()
-        # .transform_loess добавляет сглаживающую кривую
-    )
+start_btn = st.button('Magic button')
+if start_btn:
+    st.balloons()
